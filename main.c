a4b571c24b5bb0457460460a2e2c1384c086c69e
guille1093 <guille121093@gmail.com>
2022-04-20 08:39:13 -0300

    Update main.c
    ahora es dinamico

--- main.c
+++ main.c
@@ -10,57 +10,60 @@ y pasa como parametros a una funcion los arrays de estructuras
 
 
 */
-
-
-
-struct Autos
+typedef struct Autos
 {
     int patente;
     char marca[10];
-    int anio;
-};
-struct Autos ArrayAutos[3];
+    int año;
+}Autos;
+struct Autos* ArrayAutos;
 
-void BuscarXAnio(int ani, struct Autos ArrayAutos[3])
-{
-    struct Autos ArrayAutosAnio[3];
-    int j;
-    int x=0;
 
-    for(j=0; j<3; j++){
-        if(ArrayAutos[j].anio==ani){
-           ArrayAutosAnio[x]=ArrayAutos[j];
-           x++;
-        }
+//prototype
+void BuscarXaño(int, int, Autos*);
 
-    }
-    for(j=0; j<x; j++){
-        printf("patente: %d\t marca: %s\t anio: %d\n", ArrayAutosAnio[j].patente,ArrayAutosAnio[j].marca,ArrayAutosAnio[j].anio);
-    }
-
-}
 
 
 int main(void)
 {
     int i;
-    int anio;
+    int año;
+    int cant;
     printf("carga ArrayAutos\n");
-
-    for (i=0; i<3; i++){
+    printf("Ingrese la cantidad de autos a registrar\n");
+    scanf("%d",&cant);
+    ArrayAutos = malloc(sizeof(Autos) * cant);
+    for (i=0; i<cant; i++){
         printf("\n ingrese la patente: \n");
         scanf("%d",&ArrayAutos[i].patente);
         printf("\n ingrese la marca: \n");
-        scanf("%s",ArrayAutos[i].marca);
-        printf("\n ingrese el anio: \n");
-        scanf("%d", &ArrayAutos[i].anio);
+        scanf("%s", ArrayAutos[i].marca);
+        printf("\n ingrese el año: \n");
+        scanf("%d", &ArrayAutos[i].año);
 
     }
-    printf("\n Ingrese un anio, por favor\n");
-    scanf("%d",&anio);
-    BuscarXAnio(anio, ArrayAutos);
-
+    printf("\n Ingrese un año, por favor\n");
+    scanf("%d",&año);
+    BuscarXaño(año, cant, ArrayAutos);
+    free(ArrayAutos);
     return 0;
 }
+
+void BuscarXaño(int ani, int cant, Autos* ArrayAutos)
+{
+    Autos* ArrayAutosaño = alloca(sizeof(Autos)*cant);
+    int x=0;
+    for(int j=0; j<cant; j++){
+        if(ArrayAutos[j].año==ani){
+           ArrayAutosaño[x]=ArrayAutos[j];
+           x++;
+        }
+
+    }
+    for(int j=0; j<x; j++){
+        printf("patente: %d\t marca: %s\t año: %d\n", ArrayAutosaño[j].patente,ArrayAutosaño[j].marca,ArrayAutosaño[j].año);
+    }
+}
+
 
 
